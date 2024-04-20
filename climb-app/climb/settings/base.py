0b9dcbc953e3806a84491f46e6b7e8e25b3a7ef5
hyuni <wlgus7313@gmail.com>
2024-04-21 02:24:04 +0900

    fix: settings

--- climb-app/climb/settings/base.py
+++ climb-app/climb/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8o!02_f%$7)drv$1l_z^10j2o&d2@g*2-jep%#t!nub#v%i%8u"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ climb-app/climb/settings/production.py
@@ -0,0 +1,33 @@
+# flake8: noqa
+import os
+
+import environ
+
+from .base import *
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, "envs/.env.production"))
+
+SECRET_KEY = env("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": env("DATABASE_HOST"),
+        "PORT": env("DATABASE_PORT"),
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "OPTIONS": {"charset": "utf8mb4"},
+    },
+}
